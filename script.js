42b939563e42965c027265c00f36e3eba75ba8ee
Guilherme Vailatti <60483961+Guilherme-Vailatti@users.noreply.github.com>
2023-10-02 10:50:51 -0300

    novo-fluxo

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
+    console.log('funfa!')
     $('.overlay,.modal_item_header_close').click(function () {
         $('.overlay').fadeOut();
         $('.modal').fadeOut();
         $('.modal_item').slideUp();
         return false;
     });
-    $('.item,.item-mobile').click(function () {
-        $('.overlay').fadeIn();
-        $('.modal').fadeIn();
-        $('.modal_item:eq(' + $(this).index() + ')').slideDown();
-        return false;
-    });
+
+
+$('.item, .new-items, .new-items2, .item-mobile').click(function () {
+console.log('funciona')
+    $('.overlay').fadeIn();
+    $('.modal').fadeIn();
+    $('.modal_item:eq(' + $(this).index() + ')').slideDown();
+    return false;
+});
+
+
+
 });
